52c764e67e723cbcd3c96129e771234f769531f6
Shivsrijit Verma <shivsrijit@gmail.com>
2026-04-06 04:25:51 +0530

    changed app.js in fronto fix deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ledger-u9p6.onrender.com/",
         changeOrigin: true
       }
     }
