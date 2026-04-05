0fc823fef5e00a900193134181e87ebec3a606f0
Shivsrijit Verma <shivsrijit@gmail.com>
2026-04-06 04:17:52 +0530

    changed app.js in fronto fix deployment

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
-const API_BASE = "http://localhost:5000/api";
+// const API_BASE = "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_URL + "/api";
 let token = "";
 
 function print(data) {
