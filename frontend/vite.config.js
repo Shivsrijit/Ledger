91702941c2bced430955a85b3989eb0039f221d6
Shivsrijit Verma <shivsrijit@gmail.com>
2026-04-06 04:51:42 +0530

    Deployment completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://ledger-u9p6.onrender.com/",
+        target: "https://ledger-u9p6.onrender.com",
+        // target: "http://localhost:5000",
         changeOrigin: true
       }
     }
